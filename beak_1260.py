b037d162b0191cb3ae984762384eeb903edf6853
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-03-04 21:07:22 +0900

    25-03-04

--- beak_1260.py
+++ beak_1260.py
@@ -1,42 +1,45 @@
 import sys
 input = sys.stdin.readline
+
 from collections import deque
 
 n, m, v = map(int, input().split())
-# 정점의 개수만큼 행렬 생성
+
+# 1. 정점의 개수만큼 행렬 생성
 g = [[0] * (n+1) for _ in range(n+1)]
-# 정점 간의 연결을 행렬에 표시
+
+# 2. 정점 간의 연결을 행렬에 표시
 for i in range(m) :
     a,b = map(int, input().split())
     g[a][b] = g[b][a] = 1
 
-# 방문 여부 확인
-v1 = [0] * (n+1)
-v2 = v1.copy()
+# 3. 방문 여부 확인
+bfs_v = [0] * (n+1)
+dfs_v = [0] * (n+1)
 
-# 깊이 우선 탐색
-def dfs(v) : # 깊이 우선 탐색
-    v1[v] = 1 # 방문 처리
-    print(v, end = ' ')
-
-    for i in range(1, n+1) :
-        if g[v][i] == 1 and v1[i] == 0 : # 아직 방문 x, 그래프 상 연결
-            dfs(i) # 재귀 탐색
-
-# 넓이 우선 탐
+# 4-1. 너비 우선 탐색
 def bfs(v) :
-    q = deque()
-    q.append(v)
-    v2[v] = 1
+    d = deque()
+    d.append(v)
+    bfs_v[v] = 1
 
-    while q :
-        v = q.popleft()
-        print(v, end = ' ')
+    while d :
+        v = d.popleft()
+        print(v, end=' ')
 
-        for j in range(1, n+1) :
-            if v2[j] == 0 and g[v][j] == 1:
-                q.append(j)
-                v2[j] = 1
+        for j in range(1, n+1) : # 1번 정점부터 n번 정점까지 확인
+            if bfs_v[j] == 0 and g[j][v] == 1:
+                d.append(j)
+                bfs_v[j] = 1
+
+# 4-2. 깊이 우선 탐색
+def dfs(v) :
+    dfs_v[v] = 1
+    print(v, end=' ')
+
+    for k in range(1, n+1) :
+        if dfs_v[k] == 0 and g[k][v] == 1:
+            dfs(k)
 
 dfs(v)
 print()
