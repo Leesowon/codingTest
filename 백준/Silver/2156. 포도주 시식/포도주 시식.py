f0bd61b950aa6aa31da6dc8992e6962f24661225
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-03-21 15:42:35 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -2,28 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-wine = []
-for _ in range(n) :
-    wine.append(int(input()))
+wine = [int(input()) for _ in range(n)]
 
 dp = [0 for _ in range(n)]
+
 dp[0] = wine[0]
-if n > 1 : # 최소 2잔 있음
-    dp[1] = wine[0] + wine[1]
 
-if n > 2 : # 최소 3잔 있음
-    # 전전 + 현재 : w[0] + w[2]
-    # 전 + 현재 : w[1] + w[2]
-    # 전전 + 전 : w[0] + w[1]
-    dp[2] = max((wine[0] + wine[2]) , (wine[1] + wine[2]), dp[1])
+if n > 1 :
+    dp[1] = wine[0] + wine[1] # 당연히 다 마셔야..
 
-for i in range(3, n) :
-    # 1. 현재의 잔을 마시는 경우
-    # 1-1. 전전 마시고, 전 안마심 : dp[i-2] + wine[i]
-    # 1-2. 전전 안마시고, 전 마심 : dp[i-3] + wine[i-1] + wine[i]
+if n > 2 : # 최소 3잔이면, 연속되지 않게 경우의 수 체크
+    # 3번째 잔에 대해
+    # o x o
+    # x o o
+    # o o x
+    dp[2] = max((wine[0] + wine[2]), (wine[1] + wine[2]), dp[1])
 
-    # 2. 현재 잔을 안마시는 경우 : dp[i-1]
+    # o o x o
+    # o x o o
+    # ? o o x
+    for i in range(3, n) :
+        dp[i] = max((dp[i-2]+wine[i]), (dp[i-3]+wine[i-1]+wine[i]), dp[i-1])
 
-    dp[i] = max((dp[i-2] + wine[i]), (dp[i-3] + wine[i-1] + wine[i]), dp[i-1])
-
+# print(dp)
 print(dp[n-1])
