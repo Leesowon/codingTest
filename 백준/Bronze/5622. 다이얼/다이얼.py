e3436afa4167d695349faff28433b0b922cbc243
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-04-09 16:14:53 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,27 +1,11 @@
-import sys
-input = sys.stdin.readline
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 
-str_number = input().strip()
-ans_sum = 0
+alpha = list(input())		# 알파벳 단어 입력
+result = 0
 
-for sn in str_number :
-    if sn == 'A' or sn == 'B' or sn == 'C' :
-        ans_sum += 3
-    elif sn == 'D' or sn == 'E' or sn == 'F' :
-        ans_sum += 4
-    elif sn == 'G' or sn == 'H' or sn == 'I' :
-        ans_sum += 5
-    elif sn == 'J' or sn == 'K' or sn == 'L' :
-        ans_sum += 6
-    elif sn == 'M' or sn == 'N' or sn == 'O' :
-        ans_sum += 7
-    elif sn == 'P' or sn == 'Q' or sn == 'R' or sn == 'S':
-        ans_sum += 8
-    elif sn == 'T' or sn == 'U' or sn == 'V' :
-        ans_sum += 9
-    elif sn == 'W' or sn == 'X' or sn == 'Y' or sn == 'Z':
-        ans_sum += 10
-    else :
-        ans_sum += 11
-
-print(ans_sum)
+for i in alpha:
+    for j in dial:
+        if i in str(j):		# str(j) = 'A', 'B', 'C', •••
+            num = dial.index(j) + 3		# 각 알파벳 별 필요한 시간
+            result += num
+print(result)
