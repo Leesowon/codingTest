5c95835130156403f983163af2fcfd05830b655d
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-05-29 01:11:58 +0900

    [Silver III] Title: 수강신청, Time: 428 ms, Memory: 76900 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -1,20 +1,14 @@
 import sys
+input = sys.stdin.readline
 
-k, l = map(int, sys.stdin.readline().split())
+k, l = map(int, input().split())
+hak = {}
 
-# 수강신청이 들어온 순서대로 학번과 순서를 딕셔너리에 Key, Value로 저장
-dict = {}
 for i in range(l):
-    dict[sys.stdin.readline().rstrip()] = i
+    num = input().strip()  # 문자열로 입력받기 (중요!)
+    hak[num] = i  # 마지막 클릭 시점 저장
 
-# 순서를 기준으로 오름차순 정렬
-result = sorted(dict.items(), key = lambda x:x[1])
+hak_sort = sorted(hak.items(), key=lambda x: x[1])
 
-# 제한 인원보다 신청 인원이 적을 경우
-if (k > len(result)):
-    # 제한 인원을 신청 인원과 동일하게 수정
-    k = len(result)
-
-# 학번을 제한 인원만큼 출력
-for i in range(k):
-    print(result[i][0])
+for i in range(min(k, len(hak_sort))):
+    print(hak_sort[i][0])
