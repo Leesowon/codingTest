027b3be2bc7e16c0cbe6b284490faeafe6ec8da6
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-08-21 19:47:28 +0900

    [level 4] Title: 호텔 방 배정, Time: 393.84 ms, Memory: 46 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,25 +1,31 @@
+# 재귀함수로 찾아가는 부분
+# 값들간의 연결
+
+# 파이썬은 재귀함수 횟수가 2000 밖에 안됨 -> 재귀함수를 많이 할 수 있도록 설정
+import sys
+sys.setrecursionlimit(10000)
+
 def solution(k, room_number):
-    next_free = {}
     answer = []
-    next_free
     
-    for x in room_number :
-        path = []
-        # print(f"x : {x}")
+    parent = {} # 각 방 번호가 가르키는 '다음 가능한 빈 방 번호'를 저장하는 딕셔너리 - 빠른 검색은 dict가 최고!
+    
+    # union-find
+    def find(x) :
+        # x번 방이 비어있다면  (parent 기록이 없다면)
+        # x번 방을 배정하고, 다음 가능한 방 번호를 parent[x]에 저장
+        if x not in parent :
+            parent[x] = x+1
+            return x
         
-        # 이미 배정된 방이면 계속 다음 방으로 이동
-        while x in next_free :
-            # print(f"next free x :{x}")
-            path.append(x)
-            x = next_free[x]
+        # 이미 배정된 방이라면, parent[x]가 가리키는 다음 방으로 이동하여 재귀적으로 다음 탐색
+        # 이때, 경로 압축하여 중간 경로를 한번에 건더 뛸 수 있도록 설정
+        parent[x] = find(parent[x])
+        return parent[x]
         
-        # 원하던 방이 아직 배정되지 않았다면
-        answer.append(x)
-        next_free[x] = x+1
-        
-        # 경로 압축 : 지나온 방들은 한 번에 갱신 -> 원하는 방이 찼을 때 어느 방으로 배정될 것인가를 갱신
-        for p in path:
-            # print(f"path : {p}")
-            next_free[p] = x + 1
+    # 모든 고객들이 요청한 방 번호 순서대로 처리
+    for room in room_number :
+        assigned_room = find(room)
+        answer.append(assigned_room)
         
     return answer
