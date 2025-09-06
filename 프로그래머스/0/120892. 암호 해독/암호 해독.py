1853b67aa0f5f924a6c85a4f295d86658c4c5127
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-09-06 23:53:57 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,5 +1,6 @@
 def solution(cipher, code):
     answer = ''
-    for i in range(code, len(cipher)+1, code) :
-        answer += cipher[i-1]
+    # for i in range(code, len(cipher)+1, code) :
+        # answer += cipher[i-1]
+    answer = cipher[code-1::code]
     return answer
