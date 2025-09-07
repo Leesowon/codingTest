72ee5aca94d0d03b8b06a90646b55ce127553077
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-09-07 15:20:26 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = ''
+    al = {'one' : '1', 'two' : '2', 'three' : '3', 'four' : '4', 'five' : '5', 
+         'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9', 'zero' : '0'}
+    
+    num = ''
+    for s in numbers :
+        num += s
+        if num in al :
+            answer += al[num]
+            num = ''
+    
+    return int(answer)
