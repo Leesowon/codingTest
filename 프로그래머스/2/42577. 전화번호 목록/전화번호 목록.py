2dca4697b1a3fb30792751ba29f5b84ccc63f6f2
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-10-01 15:07:07 +0900

    [level 2] Title: 전화번호 목록, Time: 458.49 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,14 @@
-# https://mainkey.tistory.com/20
-
 def solution(phone_book):
     answer = True
     
-    hash_map = {}
-    for nums in phone_book :
-        hash_map[nums] = 1
+    hash = {}
+    for ph in phone_book :
+        hash[ph] = 1
     
-    for nums in phone_book :
-        arr = ""
-        for num in nums :
-            arr += num
-            
-            if arr in hash_map and arr != nums :
-                return False
-    
-    return True
+    for phone in hash :
+        tmp = ''
+        for p in phone :
+            tmp += p
+            if tmp in hash and not tmp == phone : # 자기자신은 안됨
+                answer = False
+    return answer
