16a7defb4031e77258988f4271d3d564b2e19be7
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-10-11 00:22:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 271.19 ms, Memory: 15.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,40 @@
+import heapq as hq
+
+def solution(operations):
+    answer = [0, 0]
+    
+    min_h = [] # 최소 힙
+    max_h = [] # 최대 힙
+    
+    for op in operations :
+        w, num = op.split(' ')
+        num = int(num)
+        
+        if w == 'I' :
+            hq.heappush(min_h, num)
+            hq.heappush(max_h, -num)
+            
+        else :
+            # 빈 큐에 데이터를 삭제하라는 연산이 주어지면 해당 연산은 무시
+            
+            # 최솟값 삭제
+            if num == -1 :
+                if len(min_h) == 0 :
+                    continue
+                val = hq.heappop(min_h)
+                max_h.remove(-val)
+            
+            # 최댓값 삭제
+            else : 
+                if len(max_h) == 0 :
+                    continue
+                val = hq.heappop(max_h)
+                min_h.remove(-val)
+        
+    if len(min_h) > 0 :
+        answer[1] = hq.heappop(min_h)
+
+    if len(max_h) > 0 :
+        answer[0] = -hq.heappop(max_h)
+        
+    return answer
