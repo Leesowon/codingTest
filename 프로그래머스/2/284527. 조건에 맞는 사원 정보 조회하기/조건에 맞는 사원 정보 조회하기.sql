c733231c0e5a87c800b8dfb97ee2e497bf9ee9bb
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-10-19 14:59:03 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,9 +1,8 @@
--- 코드를 작성해주세요
-select sum(score) as score, e.emp_no, e.emp_name, e.position, e.email
-from HR_EMPLOYEES as e
-inner join HR_GRADE as g on e.emp_no = g.emp_no
-# left join HR_DEPARTMENT as d on e.dept_id = d.dept_id
-group by year, g.emp_no
-having g.YEAR = '2022'
-order by 1 desc
-limit 1;
+-- 코드를
+select sum(g.score) as SCORE, g.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+from HR_EMPLOYEES as e inner join HR_GRADE as g
+on e.emp_no = g.emp_no
+group by g.emp_no, g.year
+having g.year = '2022'
+order by 1 desc # score => 1
+limit 1
