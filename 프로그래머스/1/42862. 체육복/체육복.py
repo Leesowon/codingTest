16635504839203c4eb738ae9ee6db5957d6e104f
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-10-19 14:10:13 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,17 +1,15 @@
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-    for re in reserve:
-        if re in lost:  # 본인 먼저 확인
-            lost.remove(re)
-            continue
-        elif (re - 1) in lost:
-            lost.remove(re - 1)
-            continue
-        elif (re + 1) in lost:
-            lost.remove(re + 1)
-            continue
+    answer = 0
 
-    return n - len(lost)
-
-print(solution(5, [2,4], [1,3,5]))
+    # 도난 = 여벌 거르기
+    set_lost = set(lost) - set(reserve)
+    set_reserve = set(reserve) - set(lost)
+    
+    # 빠른 탐색을 위해 정렬
+    for re in sorted(set_reserve) :
+        if (re-1) in set_lost :
+            set_lost.remove(re-1)
+        elif (re+1) in set_lost :
+            set_lost.remove(re+1)
+    
+    return n - len(set_lost)
