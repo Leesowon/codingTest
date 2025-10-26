4670165f0848bdccc2db96cf3e7136b8ceaefc45
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-10-26 20:19:55 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -3,12 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 INF = float('inf')
-dp = [INF for _ in range(n+1)]
+dp = [0] * (n+1)
 
 dp[0] = 0
 dp[1] = 1
 
 for i in range(2, n+1) :
-    dp[i] = min(dp[i], dp[i-1] + dp[i-2])
+    dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[n])
