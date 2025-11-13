0137bdb88d582307e8a94108f63270739a1dbe15
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-11-13 21:18:44 +0900

    [Gold V] Title: 토마토, Time: 3572 ms, Memory: 52364 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-M,N,H = map(int, input().split()) # m:가로, n:세로, h:높이
+M, N, H = map(int, input().split()) # 가로, 세로, 높이
 
 dh = [-1, 1, 0, 0, 0, 0]
 dn = [0, 0, -1, 1, 0, 0]
@@ -18,21 +18,22 @@ for h in range(H) :
         layer.append(tomato)
 
         for m, val in enumerate(tomato) :
-            if val == 1 : # 익은 토마토면
-                d.append((h, n, m, 0)) # 높이, 가로, 세로, day
+            if val == 1 :
+                d.append((h, n, m, 0)) # 높이, 세로, 가로, day
     box.append(layer)
 
 ans = 0
 
-# 우선 다 덱에 넣고, 주변 토마토를 한 번에 익게 해야한다.
 while d :
     h, r, c, day = d.popleft()
-    ans = max(ans, day)
+    ans = max(day, ans)
 
     for k in range(6) :
-        nh, nr, nc = h + dh[k], r + dn[k], c + dm[k]
+        nh = h + dh[k]
+        nr = r + dn[k]
+        nc = c + dm[k]
 
-        if 0 <= nh < H and 0 <= nr < N and 0 <= nc < M:
+        if 0 <= nh < H and 0 <= nr < N and 0 <= nc < M :
             if box[nh][nr][nc] == 0 :
                 box[nh][nr][nc] = 1
                 d.append((nh, nr, nc, day+1))
