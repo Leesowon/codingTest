e5c0a585db744a219bbabce64c53aa176b68007e
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-11-26 11:44:22 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -5,24 +5,17 @@ tc = int(input())
 
 for _ in range(tc) :
     n = int(input())
-    
-    if n == 1 :
-        print(1)
-        continue
-    elif n == 2 : 
-        print(2)
-        continue
-    elif n == 3 :
-        print(4) 
-        continue
 
-    dp = [0 for _ in range(n+1)]
-    
+    dp = [0] * (n+1)
+
     dp[1] = 1
-    dp[2] = 2 # 1+1, 2
-    dp[3] = 4
 
-    for i in range(4, n+1) :
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    if n > 1 :
+        dp[2] = 2
+    if n > 2 :
+        dp[3] = 4
+    if n > 3 :
+        for i in range(4, n+1) :
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
     print(dp[n])
