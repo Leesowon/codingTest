37f83eb452fdbfd71b395fb03faa76d1a31a7a0c
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-12-01 11:06:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 232 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,15 +1,13 @@
 import sys
-input = sys.stdin.readline
+input= sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
 
-dp = [0 for i in range(n+1)] # 1~n까지의 합
-dp[1] = arr[0]
+dp = [0] * (n+1) # i : 0 ~ i까지 합
 
-if n > 1:
-    for i in range(2, n+1) :
-        dp[i] = dp[i-1] + arr[i-1]
+for i in range(1, len(numbers)+1) :
+    dp[i] = dp[i-1] + numbers[i-1]
 
 for _ in range(m) :
     i, j = map(int, input().split())
