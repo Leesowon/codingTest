703488abbd1c7eccb221c0e01f5c04b07af568b8
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-12-03 19:55:34 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -2,7 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [0] * (n+1)
+
+INF = 1e9
+dp = [INF] * (n+1)
 
 dp[0] = 0
 dp[1] = 1
@@ -10,8 +12,8 @@ dp[1] = 1
 if n > 1 :
     dp[2] = 3
 
-if n > 2 :
-    for i in range(3, n+1) :
-        dp[i] = dp[i-1] + 2*dp[i-2]
+for i in range(3, n+1) :
+    # i-1에 1x2 붙이기 (1가지) + i-2에 2x1 2개 or 2x2 1개 붙이기 (2가지)
+    dp[i] = dp[i-2] * 2 + dp[i-1]
 
 print(dp[n] % 10007)
