db800b87197045d56a11654f92c353bcc325a89f
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2026-02-20 00:34:52 +0900

    [Silver II] Title: 스택 수열, Time: 3624 ms, Memory: 37852 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+nums = []
+# 수열
+for _ in range(n) :
+    nums.append(int(input()))
+
+stack = []
+ans = []
+for num in range(1, n+1) :
+    # 우선 stack에 넣기
+    stack.append(num)
+    ans.append('+')
+
+    if len(stack) < 1:
+        continue
+
+    while len(stack) > 0 and nums[0] <= stack[-1] :
+        del nums[0]
+        stack.pop()
+        ans.append('-')
+
+if len(nums) == 0 :
+    print('\n'.join(ans))
+else :
+    print('NO')
